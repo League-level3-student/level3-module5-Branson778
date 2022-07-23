3dadb7fb2810a571bb904cfb70a879507cff870e
r_boo <r_boo@192.168.1.98>
2022-07-23 16:44:05 -0700

    Finished

--- src/_02_RecursionMath/RecursionMathTest.java
+++ src/_02_RecursionMath/RecursionMathTest.java
@@ -10,7 +10,9 @@ class RecursionMathTest {
     @Test
     public void testMultiplication() {
         assertEquals(12, RecursionMath.recursiveMultiplication(3, 4));
-        
+        assertEquals(24, RecursionMath.recursiveMultiplication(4, 6));
+        assertEquals(36, RecursionMath.recursiveMultiplication(6, 6));
+        assertEquals(144, RecursionMath.recursiveMultiplication(12, 12));
         // Add more JUnit tests like the one above to test your method
         
     }
@@ -18,12 +20,20 @@ class RecursionMathTest {
     @Test
     public void testDivision() {
         // Add JUnit tests to test your method
-        fail("Write some tests!");
+        assertEquals(4, RecursionMath.recursiveDivision(24, 6));
+        assertEquals(2, RecursionMath.recursiveDivision(12, 6));
+        assertEquals(5, RecursionMath.recursiveDivision(60, 12));
+        assertEquals(3, RecursionMath.recursiveDivision(27, 9));
+    	//fail("Write some tests!");
     }
 
     @Test 
     public void testPower() {
         // Add JUnit tests to test your method
-        fail("Write some tests!");
+    	assertEquals(25, RecursionMath.recursivePower(5, 2));
+    	assertEquals(125, RecursionMath.recursivePower(5, 3));
+    	assertEquals(8, RecursionMath.recursivePower(2, 3));
+    	assertEquals(343, RecursionMath.recursivePower(7, 3));
+        //fail("Write some tests!");
     }
 }
